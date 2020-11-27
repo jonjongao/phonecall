96bf175d5f119663c311317cc1655de4c87f552e
Jon Gao <jonjon.liho@gmail.com>
2020-11-28 00:55:01 +0800

    update style, load and reload process

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 var isShow = false;
+var player;
 
 window.addEventListener("load", function () {
-  //videojs(document.querySelector('.video-js'));
-  videojs("player", {
-    controls: true,
-    autoplay: true,
-    preload: "auto",
-    width: 214,
-    height: 330,
-    poster: "adventure_thumbnail.jpg",
-    fluid: true,
-    aspectRatio: "9:16"
-  });
+  videojs("player");
 
   videojs("player").ready(function () {
+    player = this;
     // this.play();
-    
-    this.on('timeupdate',function(){
-      console.log(this.currentTime());
-      let t=this.currentTime();
 
-      if(t >= 10)
-      {
-        if(isShow==false)
-        {
+    this.on("timeupdate", function () {
+      // console.log(this.currentTime());
+      let t = this.currentTime();
+
+      if (t >= 10 && t < 15) {
+        if (isShow == false) {
           this.src("call_test.mp4");
-          console.log("change video");
-          isShow=true;
+          // console.log("change video");
+          isShow = true;
         }
       }
     });
-  });
 
-  
+    let btn = document.getElementsByClassName("vjs-play-control")[0];
+    btn.onclick = function () {
+      let type = btn.getAttribute("title");
+      if (type == "Replay") {
+        player.load();
+        fetchVideoAndPlay();
+      }
+    };
+  });
 });
+
+function fetchVideoAndPlay() {
+  fetch("./call_test.mp4")
+    .then((response) => response.blob())
+    .then((blob) => {
+      video.srcObject = blob;
+      return video.play();
+    })
+    .then((_) => {
+      // Video playback started ;)
+    })
+    .catch((e) => {
+      player.src("adventure.mp4");
+      isShow = false;
+      // Video playback failed ;(
+    });
+}
