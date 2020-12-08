1a042da7fae0ea762c16b6361138b310b376209b
jonjongao <jonjon.liho@gmail.com>
2020-12-08 17:16:12 +0800

    fix video playing, add minify js

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
+var isFirstContact = true;
 var isShow = false;
 var player;
 
 window.addEventListener("load", function () {
-  videojs("player");
+  player = videojs("player", {}, function () {
+    // let btn1 = document.getElementsByClassName("vjs-big-play-button")[0];
+    // let btn2 = document.getElementsByClassName("vjs-play-control")[0];
 
-  videojs("player").ready(function () {
-    player = this;
-    // this.play();
+    // player.on("click", function (e) {
+    //   /**
+    //    * ! 點擊影片任一處
+    //    */
+    //   // console.log(e);
+    //   // console.log("click");
+    // });
 
-    this.on("timeupdate", function () {
+    player.on("play", checkFirstContact);
+
+    /**
+     * ! 鬼來電
+     */
+    player.on("timeupdate", function () {
       // console.log(this.currentTime());
       let t = this.currentTime();
 
       if (t >= 10 && t < 15) {
         if (isShow == false) {
-          this.src("call_test.mp4");
-          // console.log("change video");
+          player.src({ type: "video/mp4", src: "call_test.mp4" });
+          player.load();
+          player.play();
           isShow = true;
+
+          player.on("ended", onCallEnded);
+
+          gtag("event", "video_call_in", {
+            event_category: "影片操作",
+            event_label: "鬼來電",
+            non_interaction: true
+          });
         }
       }
     });
-
-    let btn = document.getElementsByClassName("vjs-play-control")[0];
-    btn.onclick = function () {
-      let type = btn.getAttribute("title");
-      if (type == "Replay") {
-        player.load();
-        fetchVideoAndPlay();
-      }
-    };
   });
 });
+
+function checkFirstContact() {
+  if (isFirstContact) {
+    // console.log("play");
+    gtag("event", "video_play", {
+      event_category: "影片操作",
+      event_label: "主動播放",
+      non_interaction: true
+    });
+    isFirstContact = false;
+    player.off("play", checkFirstContact);
+  }
+}
+
+function onCallEnded() {
+  // console.log("鬼來電結束");
+  player.off("ended", onCallEnded);
+  player.on("play", restartFromCall);
+}
+
+function restartFromCall() {
+  player.off("play", restartFromCall);
+  gtag("event", "video_replay", {
+    event_category: "影片操作",
+    event_label: "重播",
+    non_interaction: true
+  });
+  // console.log("從鬼來電重新開始");
+  player.load();
+  fetchVideoAndPlay();
+}
 
 function fetchVideoAndPlay() {
   fetch("./call_test.mp4")
@@ -43,7 +86,9 @@ function fetchVideoAndPlay() {
       // Video playback started ;)
     })
     .catch((e) => {
-      player.src("adventure.mp4");
+      player.src({ type: "video/mp4", src: "adventure.mp4" });
+      player.load();
+      player.play();
       isShow = false;
       // Video playback failed ;(
     });
